00d1e4621bfdba9f88bd8e6c2ec5086b5a988533
Eugene Stanko <j3ck.dev@gmail.com>
2017-02-08 02:55:12 +0300

    cache photos search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,18 @@ class SearchController < ApplicationController
   def index
     query = params[:query] || 'random'
     page = params[:page] || 0
-    @results = FLICKR.photos.search(text: query, page: page, per_page: 50)
+    @results = cached_search_result(query, page)
   end
 
   def show
     @photo = FLICKR.photos.find_by_id(params['id'])
+  end
+
+  private
+
+  def cached_search_result(query, page)
+    Rails.cache.fetch("#{query}-#{page}", expires_in: 1.hour) do
+      FLICKR.photos.search(text: query, page: page, per_page: 50)
+    end
   end
 end
