24110b0ad5ecf1475571207bee567435b4621da5
Eugene Stanko <j3ck.dev@gmail.com>
2017-02-08 02:54:17 +0300

    simple pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
     query = params[:query] || 'random'
-    @results = FLICKR.photos.search(text: query)
+    page = params[:page] || 0
+    @results = FLICKR.photos.search(text: query, page: page, per_page: 50)
   end
 
   def show
