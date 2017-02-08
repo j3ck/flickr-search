b21ffef9a97be7bda80281f4dfe5f98edadfedf0
Eugene Stanko <j3ck.dev@gmail.com>
2017-02-08 14:08:57 +0300

    remove lucky cache, process 'show' find

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,14 @@
+# TODO: implement requests cache in Flickr fu
 class SearchController < ApplicationController
   def index
     query = params[:query] || 'random'
     page = params[:page] || 0
-    @results = cached_search_result(query, page)
+    @results = FLICKR.photos.search(text: query, page: page, per_page: 50)
   end
 
   def show
     @photo = FLICKR.photos.find_by_id(params['id'])
-  end
-
-  private
-
-  def cached_search_result(query, page)
-    Rails.cache.fetch("#{query}-#{page}", expires_in: 1.hour) do
-      FLICKR.photos.search(text: query, page: page, per_page: 50)
-    end
+  rescue
+    redirect_to root_path
   end
 end
